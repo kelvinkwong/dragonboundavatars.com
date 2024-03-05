cb862aff7f1b406684698b10943ab048aefca59d
kelvin wong <kelvinwong@outlook.com.au>
2024-03-05 21:40:35 +1100

    add config

---
+++ docs/scripts/config.js
@@ -0,0 +1,3 @@
+const ID_SELECTED_TABLE = 'selected_table';
+const ID_SELECTED_ROW = 'selected_row';
+const URLS = ['./cache/body.html', './cache/flags.html', './cache/glasses.html', './cache/hats.html'];
